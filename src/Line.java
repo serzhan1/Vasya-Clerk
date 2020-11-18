bec09b0a4075ac698c2ca1e093ea658e12cd23ba
seniorserzhan@gmail.com <seniorserzhan@gmail.com>
2020-11-19 02:28:59 +0600

    Refactoring

--- src/Line.java
+++ src/Line.java
@@ -1,28 +1,28 @@
 public class Line {
     public static String Tickets(int[] peopleInLine)
     {
-        int cost25 =0, cost50=0, cost100 = 0;
+        int bill25 =0, bill50=0, bill100 = 0;
         for(int i = 0; i < peopleInLine.length; i++)
         {
             if(peopleInLine[i] == 25)
-                cost25++;
+                bill25++;
             else if(peopleInLine[i] == 50)
             {
-                if(cost25 > 0){
-                    cost25--;
-                    cost50++;}
+                if(bill25 > 0){
+                    bill25--;
+                    bill50++;}
                 else
                     return "NO";
             }
             else if(peopleInLine[i] == 100)
             {
-                if(cost25 > 0 && cost50 > 0)
+                if(bill25 > 0 && bill50 > 0)
                 {
-                    cost25--;
-                    cost50--;
+                    bill25--;
+                    bill50--;
                 }
-                else if(cost25 > 2){
-                    cost25 = cost25 - 3;
+                else if(bill25 > 2){
+                    bill25 = bill25 - 3;
                 }
                 else
                     return "NO";
